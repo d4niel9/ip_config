25e35fb788de7f0ddeaef78a7724d443bdb47bce
daniel <100847059+d4niel9@users.noreply.github.com>
2022-12-09 20:07:58 -0600

    script

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+import time
+
+
+def main():
+    try:
+        command_init = "kalitorify -t"
+        os.popen(command_init)
+        time.sleep(9) #Define switch init time in seconds
+        while True:
+            command_switch_ip = "kalitorify -r"
+            os.popen(command_switch_ip)
+            time.sleep(15) #Define renew time in seconds
+    except KeyboardInterrupt as err:
+        print("Run "sudo kalitorify -c" manually to stop tor")
+
+
+if __name__ == "__main__":
+    main()
