dfac6ee257411c77a0b8b469fadf39fec7b4172a
daniel <100847059+d4niel9@users.noreply.github.com>
2022-12-09 20:17:26 -0600

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def main():
             os.popen(command_switch_ip)
             time.sleep(15) #Define renew time in seconds
     except KeyboardInterrupt as err:
-        print("Run "sudo kalitorify -c" manually to stop tor")
+        print("""Run "sudo kalitorify -c" manually to stop tor""")
 
 
 if __name__ == "__main__":
